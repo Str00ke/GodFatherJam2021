25f9edd7c88eebb1e9c224f83a5ac2cbaf71fc61
MrFrkl1 <55458843+MrFrkl1@users.noreply.github.com>
2021-09-07 17:50:06 +0200

    Scene

---
+++ TOI TU CREUSES/Assets/Scripts/GridManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+
+    public GameObject square;
+    [SerializeField]
+    int _height, _length;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int k = 0; k < _length; k++)
+        {
+            for (int j = 0; j < _height; j++)
+            {
+                GameObject block = Instantiate(square, new Vector2((k - _length / 2), (j - _height / 2)), Quaternion.identity);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ TOI TU CREUSES/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : PlayerController
+{
+    // Digger
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void Movement()
+    {
+        base.Movement();
+    }
+    protected override void DropBomb() { }
+}

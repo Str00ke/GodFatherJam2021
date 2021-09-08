650b1d139ed210e5129d535220613d6312d8f945
Str00ke <charlelie.garrigues@edu.devinci.fr>
2021-09-08 16:20:05 +0200

    Add camera responsive to terrain, spawn turrets and player nice, and plein d'autres things.

--- TOI TU CREUSES/Assets/Scripts/CamAdapt.cs
+++ TOI TU CREUSES/Assets/Scripts/CamAdapt.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class CamAdapt : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject farestObjTmp;
+    public GameObject turret;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void CamAdaptToTerrain(GameObject farestObj, float sizeX, float sizeY)
+    {
+        GameObject helper = new GameObject();
+        helper.AddComponent<SpriteRenderer>();
+        helper.transform.position = new Vector2(farestObj.GetComponent<Renderer>().bounds.min.x - turret.GetComponent<Collider2D>().bounds.size.x * 2.5f, farestObj.GetComponent<Renderer>().bounds.max.y + turret.GetComponent<Collider2D>().bounds.size.y * 2.5f);
+        farestObjTmp = helper;
+        if (sizeY > sizeX)
+        {
+            RecAdapt(helper, sizeY);
+        } else
+        {
+            RecAdapt(helper, sizeX);
+        }
         
     }
+
+
+    void RecAdapt(GameObject obj, float size)
+    {
+        if (!obj.GetComponent<Renderer>().isVisible)
+        {
+            GetComponent<Camera>().orthographicSize += Time.deltaTime * (size / 6);
+            StartCoroutine(AdaptTimer(size));
+        }
+        else 
+        {
+            FindObjectOfType<TouretsManager>().PlaceTurrets();
+            FindObjectOfType<GridManager>().SpawnDigger();
+        }
+            
+    }
+
+    IEnumerator AdaptTimer(float size)
+    {
+        yield return new WaitForSeconds(Time.deltaTime);
+        RecAdapt(farestObjTmp, size);
+    }
+    
 }
